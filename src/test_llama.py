393cce576fd8eb64305c48fd711c802d33110705
Adam Klein <adam3207@gmail.com>
2026-01-15 00:50:17 +0000

    jhjkh

--- src/test_llama.py
+++ src/test_llama.py
@@ -1,4 +1,8 @@
 
+
+import torch_xla
+import torch_xla.core.xla_model as xm
+
 import hydra
 import omegaconf
 
@@ -8,7 +12,10 @@ from utils.import_utils import import_model
 @hydra.main(version_base=None, config_path="configs", config_name="default")
 def main(config: omegaconf.DictConfig):
 
-    model = import_model(config.model.model_class)(config.model)
+    with torch_xla.device():
+        model = import_model(config.model.model_class)(config.model)
+
+    xm.rendezvous("load_model")
 
     print(f"Model initialized: {config.model.model_class}")
     print(f"Total parameters: {sum(p.numel() for p in model.parameters()):_}")
