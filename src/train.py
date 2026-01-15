5981cb2df986890656005d8c6c0e1722ec72aa8d
Adam Klein <adam3207@gmail.com>
2026-01-15 04:51:03 +0000

    uh

--- src/train.py
+++ src/train.py
@@ -84,7 +84,7 @@ def main(config: omegaconf.DictConfig):
             remove_folder=True
         )
 
-    print(model.lm_head.device)
+    print(model.lm_head.weight.data.device)
     model = model.to(torch_xla.device())
 
     xm.rendezvous("Model Initialization")
