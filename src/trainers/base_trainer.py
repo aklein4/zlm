944879242acb0cca7f3a9b2f3eb11891a0297a71
Adam Klein <adam3207@gmail.com>
2026-01-22 03:27:00 +0000

    lr_scheduler kwargs

--- src/trainers/base_trainer.py
+++ src/trainers/base_trainer.py
@@ -169,8 +169,7 @@ class BaseTrainer:
         lr_scheduler = get_scheduler(
             name=config.trainer.lr_scheduler.type,
             optimizer=optimizer,
-            num_warmup_steps=config.trainer.lr_scheduler.warmup_steps,
-            num_training_steps=config.trainer.lr_scheduler.training_steps,
+            scheduler_specific_kwargs=config.trainer.lr_scheduler.kwargs,
         )
 
         return optimizer, lr_scheduler
