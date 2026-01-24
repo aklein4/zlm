cf5446a356fab67c74c47bb77a7e2e4e1c246d4c
Adam Klein <adam3207@gmail.com>
2026-01-24 20:27:23 +0000

    batchnorm module and simple single-pass asym

--- src/utils/torch_modules.py
+++ src/utils/torch_modules.py
@@ -66,4 +66,88 @@ class ContinuousEmbedding(nn.Module):
         emb = torch.cat([emb_sin, emb_cos], dim=-1)
 
         return self.proj(emb)
+
+
+class UnbiasedEMA(nn.Module):
+
+    def __init__(
+        self,
+        shape: torch.Size,
+        beta: float,
+        eps: float=1e-8,
+    ):
+        super().__init__()
+
+        self.shape = shape
+        self.beta = beta
+        self.eps = eps
+
+        self.register_buffer(
+            'num_updates', torch.zeros(1), persistent=True
+        )
+        self.register_buffer(
+            'weight', torch.zeros(shape), persistent=True
+        )
+
+
+    @torch.no_grad()
+    def update(self, x: torch.Tensor) -> None:
+        assert x.shape == self.shape, f"Input shape {x.shape} does not match EMA shape {self.shape}"
+       
+        self.num_updates += 1
+        self.weight.mul_(self.beta).add_(
+            x.detach().to(self.weight.dtype), alpha=1 - self.beta
+        )
+
+
+    @torch.no_grad()
+    def retrieve(self) -> torch.Tensor:
+        bias_correction = 1 - self.beta ** self.num_updates
+        return self.weight / (bias_correction + self.eps)
+
+
+class CustomBatchNorm(nn.Module):
+
+    def __init__(
+        self,
+        shape: torch.Size,
+        beta: float,
+        eps: float=1e-5,
+    ):
+        super().__init__()
+
+        self.shape = shape
+        self.beta = beta
+        self.eps = eps
+
+        self.mean_tracker = UnbiasedEMA(
+            shape, beta, eps
+        )
+        self.var_tracker = UnbiasedEMA(
+            shape, beta, eps
+        )
+
+    
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        assert x.shape[1:] == self.shape, f"Input shape {x.shape} does not match BatchNorm shape {self.shape}"
+
+        og_dtype = x.dtype
+        x = x.to(torch.float32)
+
+        if self.training:
+            self.mean_tracker.update(
+                x.mean(dim=0)
+            )
+            self.var_tracker.update(
+                x.var(dim=0, unbiased=False)
+            )
+
+        mean = self.mean_tracker.retrieve()
+        std = torch.sqrt(
+            self.var_tracker.retrieve() + self.eps
+        )
+
+        y = (x - mean[None]) / std[None]
+
+        return y.to(og_dtype)
     
