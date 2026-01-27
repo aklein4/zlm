90c19deeac752f6518dabf7ca5dc904b829f2998
Adam Klein <adam3207@gmail.com>
2026-01-27 21:44:04 +0000

    initial batchnorm

--- src/models/zlm.py
+++ src/models/zlm.py
@@ -19,7 +19,7 @@ from utils.torch_utils import (
 from models.llama import LlamaForCausalLM
 from models.custom_llama import LlamaMLP, LlamaRMSNorm, LlamaDecoderLayer, CustomLlamaModel
 from models import load_checkpoint_state
-from utils.torch_modules import ScaledEmbedding
+from utils.torch_modules import ScaledEmbedding, InitialBatchNorm
 import utils.constants as constants
 
 
@@ -438,6 +438,12 @@ class ZLMModel(nn.Module):
         # self.mu_out_norm = LlamaRMSNorm(self.latent_size, eps=config.rms_norm_eps, elementwise_affine=False)
         self.z_in_norm = LlamaRMSNorm(self.latent_size, eps=config.rms_norm_eps, elementwise_affine=False)
 
+        # create an initializing batch norm for the mu output
+        self.mu_initial_batch_norm = InitialBatchNorm(
+            [self.z_length, self.latent_size],
+            eps=config.rms_norm_eps,
+        )
+
         # create the diffusion components
         self.diffusion_head = DiffusionHead(config)
         self.scheduler = DiffusionScheduler(config)
@@ -462,13 +468,12 @@ class ZLMModel(nn.Module):
         self.apply(self.scaled_embed_init)
 
         # scale input layers by embedding stats
-        # self.encoder_noise_proj_in.weight.data.zero_()
-        self.encoder_noise_proj_in.weight.data *= embed_std[:, None]
+        self.encoder_noise_proj_in.weight.data.zero_()
         self.decoder_z_proj_in.weight.data *= embed_std[:, None]
 
         # initialize the diffusion heads with small values
-        # self.diffusion_head.out_proj.weight.data.mul_(config.diffusion_output_init_scale)
-        # self.uncond_diffusion_head.out_proj.weight.data.mul_(config.diffusion_output_init_scale)
+        self.diffusion_head.out_proj.weight.data.mul_(config.diffusion_output_init_scale)
+        self.uncond_diffusion_head.out_proj.weight.data.mul_(config.diffusion_output_init_scale)
 
 
     def scaled_embed_init(self, module):
@@ -561,6 +566,10 @@ class ZLMModel(nn.Module):
         mu = self.encoder_mu_proj_out(
             hidden_states[..., -self.z_length:, :]
         )
+
+        # apply batch norm then rms norm
+        mu = self.mu_initial_batch_norm(mu)
+        # mu = self.mu_out_norm(mu)
 
         z = self.scheduler.add_noise(
             mu, torch.zeros(1, dtype=torch.long, device=mu.device), noise
