cc68210759516c6c0439dfc49baf2a3e6fefbfe1
Adam Klein <adam3207@gmail.com>
2026-02-02 01:49:58 +0000

    adklvjn

--- src/train.py
+++ src/train.py
@@ -4,7 +4,8 @@ import os
 os.environ['PJRT_DEVICE'] = 'TPU'
 os.environ['XLA_IR_DEBUG'] = '1'
 os.environ['XLA_HLO_DEBUG'] = '1'
-os.environ['XLA_FLAGS']=  "--xla_dump_to=xla_dump --xla_dump_hlo_snapshots"
+os.environ['XLA_SAVE_TENSORS_FILE'] = 'xla_debugging/xla_ir_dump.txt'
+os.environ['XLA_SAVE_TENSORS_FMT'] = 'hlo'
 
 import logging
 import sys
