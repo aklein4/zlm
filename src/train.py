5a99182e7f043741402579896b458ddb67a61b29
Adam Klein <adam3207@gmail.com>
2026-02-02 01:40:11 +0000

    dump the hlo snapshots

--- src/train.py
+++ src/train.py
@@ -2,6 +2,9 @@
 
 import os
 os.environ['PJRT_DEVICE'] = 'TPU'
+os.environ['XLA_IR_DEBUG'] = '1'
+os.environ['XLA_HLO_DEBUG'] = '1'
+os.environ['XLA_FLAGS']=  "--xla_dump_to=~/xla_dump --xla_dump_hlo_snapshots"
 
 import logging
 import sys
