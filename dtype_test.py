91dd8e09dc95aa7478291e18f7b3489ec5a4962e
Adam Klein <adam3207@gmail.com>
2026-02-25 19:07:52 +0000

    on xla

--- dtype_test.py
+++ dtype_test.py
@@ -4,11 +4,14 @@ import torch_xla
 
 def main():
     
-    x = torch.tensor([1, 2, 3], dtype=torch.float32)
-    y = torch.tensor([4, 5, 6], dtype=torch.bfloat16)
+    x = torch.tensor([1, 2, 3], dtype=torch.float32).to('xla')
+    y = torch.tensor([4, 5, 6], dtype=torch.bfloat16).to('xla')
 
     z = x + y
-    print(z.dtype)
+
+    torch_xla.sync()
+
+    print(z)
 
 
 if __name__ == "__main__":
