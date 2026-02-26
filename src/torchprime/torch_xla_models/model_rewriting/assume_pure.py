e90a7430924930a3c0eb48c6992c4451b5d0302e
Adam Klein <adam3207@gmail.com>
2026-02-26 01:21:50 +0000

    pure modules

--- src/torchprime/torch_xla_models/model_rewriting/assume_pure.py
+++ src/torchprime/torch_xla_models/model_rewriting/assume_pure.py
@@ -31,19 +31,20 @@ def mark_pure_modules(model: nn.Module, config: DictConfig) -> nn.Module:
     Transformed model.
   """
   pure_module_config = config.model.pure_modules
-  if pure_module_config:
-    torch_xla_version = torch_xla.version.__version__
-    if torch_xla_version.startswith("2.7"):
-      logger.warning("pure_modules is not supported for PyTorch/XLA 2.7.x")
-      return model
-    if torch_xla_version == "2.8.0":
-      logger.warning("pure_modules is not supported for PyTorch/XLA 2.8.0")
-      return model
+  # if pure_module_config:
+  #   torch_xla_version = torch_xla.version.__version__
+  #   if torch_xla_version.startswith("2.7"):
+  #     logger.warning("pure_modules is not supported for PyTorch/XLA 2.7.x")
+  #     return model
+  #   if torch_xla_version == "2.8.0":
+  #     logger.warning("pure_modules is not supported for PyTorch/XLA 2.8.0")
+  #     return model
 
   pure_module_classes = get_classes_by_names(model, pure_module_config)
 
   def transform(mod: nn.Module, _: str):
     if isinstance(mod, pure_module_classes):
+      print(f"Wrapping module {mod} in PureModule")
       return PureModule(mod)
     return mod
 
