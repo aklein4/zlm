c529e5ed28469a5ad03ded23a1d086c565474e01
Adam Klein <adam3207@gmail.com>
2026-03-05 21:48:30 +0000

    python 3.11

--- tpu_setup.sh
+++ tpu_setup.sh
@@ -13,21 +13,27 @@ Example:
     . tpu_setup.sh <HF_ID> <HF_TOKEN> <WANDB_TOKEN>
 '
 
+# boilerplate
+sudo apt-get update -y
+
+# install python 3.11
+sudo apt install -y python3.11 python3.11-venv python3.11-dev python3.11-pip
+
 # update path(?)
 export PATH="/home/$USER/.local/bin:$PATH"
 
 # # upgrade and update pip
-pip install --upgrade pip
-pip install --upgrade setuptools
+python3.11 -m pip --upgrade pip
+python3.11 -m pip --upgrade setuptools
 
 # install torch
-pip install torch==2.9.0 --index-url https://download.pytorch.org/whl/cpu
-pip install torch_xla[tpu]==2.9.0
-pip install --pre torch_xla[pallas]==2.9.0 --index-url https://us-python.pkg.dev/ml-oss-artifacts-published/jax/simple/ --find-links https://storage.googleapis.com/jax-releases/libtpu_releases.html
+python3.11 -m pip torch==2.9.0 --index-url https://download.pytorch.org/whl/cpu
+python3.11 -m pip torch_xla[tpu]==2.9.0
+python3.11 -m pip --pre torch_xla[pallas]==2.9.0 --index-url https://us-python.pkg.dev/ml-oss-artifacts-published/jax/simple/ --find-links https://storage.googleapis.com/jax-releases/libtpu_releases.html
 
 # install extras
-pip install setuptools==67.7.2
-pip install -r tpu_requirements.txt
+python3.11 -m pip setuptools==67.7.2
+python3.11 -m pip -r tpu_requirements.txt
 
 # login to huggingface
 hf auth login --token $2
