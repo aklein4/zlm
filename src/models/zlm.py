8542fc020869a8c8ad6079770f0d6fc6d368476e
Adam Klein <adam3207@gmail.com>
2026-03-05 21:29:48 +0000

    fix import

--- src/models/zlm.py
+++ src/models/zlm.py
@@ -22,7 +22,7 @@ from utils.torch_utils import (
 from models.llama import LlamaForCausalLM, LlamaMLP, LlamaRMSNorm
 from models.custom_llama import CustomLlamaModel, CustomLlamaDecoderLayer
 from models import load_checkpoint_state
-from utils.torch_modules import ScaledEmbedding, SpectralBatchNorm, OnceSpectralBatchNorm, CustomBatchNorm, UnbiasedEMA
+from utils.torch_modules import SpectralBatchNorm, OnceSpectralBatchNorm, CustomBatchNorm, UnbiasedEMA
 from utils.diffusion_utils import DiffusionScheduler
 
 
