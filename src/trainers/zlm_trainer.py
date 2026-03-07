correct mi calculation

src/trainers/zlm_trainer.py
@@ -105,7 +105,7 @@ class ZLMTrainer(BaseTrainer):
 
         dists = torch.cdist(mu, mu, p=2) # [S, B, B]
 
-        kls = (self.model.scheduler.a[0] * dists.pow(2)) / (2 * self.model.scheduler.b[0].pow(2))
+        kls = (self.model.scheduler.a[0] * dists).pow(2) / (2 * self.model.scheduler.b[0].pow(2))
         logp = torch.log_softmax(-kls, dim=-1) # [S, B, B]
 
         mi = torch.diagonal(logp, dim1=-2, dim2=-1).mean() - np.log(1 / logp.shape[-1])