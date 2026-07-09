d2637381ecc29fe90cf2584c9fea032f2f37eef1
Adam Klein <adam3207@gmail.com>
2026-07-09 20:54:24 +0000

    attribute error

--- src/trainers/zlm_trainer.py
+++ src/trainers/zlm_trainer.py
@@ -74,7 +74,7 @@ class ZLMTrainer(BaseTrainer):
             ) / x.shape[1] # [S, H, H]
 
             v = torch.linalg.eigvalsh(
-                cov + self.model.mu_out_norm.eps * torch.eye(x.shape[-1], device=x.device, dtype=cov.dtype)[None]
+                cov + self.model.config.rms_norm_eps * torch.eye(x.shape[-1], device=x.device, dtype=cov.dtype)[None]
             ) # [S, H]
 
         spectral_reg = (v.pow(2)/2 - v.log() - 1/2).mean()
