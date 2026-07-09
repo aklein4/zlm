b01b338516fab59834b070a8779986751566e972
Adam Klein <adam3207@gmail.com>
2026-07-09 21:44:59 +0000

    no backprop through eigvals

--- src/trainers/zlm_trainer.py
+++ src/trainers/zlm_trainer.py
@@ -80,7 +80,7 @@ class ZLMTrainer(BaseTrainer):
         spectral_reg = (v.pow(2)/2 - v.log() - 1/2).mean()
         spectral_parties = self.get_effective_parties(v)
 
-        return spectral_reg, spectral_parties
+        return spectral_reg.detach(), spectral_parties.detach()
 
 
     def kl_loss(
